9ef57f2c7f633cb49712c29c0c8444bfb2826c8b
gobuperez <gobuperez@gmail.com>
2024-03-18 13:51:53 +0100

    Reducción en el número de peticiones que realiza el buscador de universidades

--- src/components/UniFinder.js
+++ src/components/UniFinder.js
@@ -1,6 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 import { BuildingLibraryIcon } from '@heroicons/react/24/outline';
 import { Combobox } from '@headlessui/react';
@@ -11,14 +11,31 @@ const UniFinder = () => {
 
     const router = useRouter();
     
+    const [searchQuery, setSearchQuery] = useState('');
     const [universities, setUniversities] = useState([]);
 
-    const searchUni = async (name) => {
+    useEffect(() => {
 
-        const response = await APIConnection('GET', `/universities?search=${name}`);
-        
-        if (response.status === 200) setUniversities(response.universities);
-        else setUniversities([]);
+        const delayDebounceFn = setTimeout( async () => {
+
+            if (searchQuery.length > 0) {
+
+                const response = await APIConnection('GET', `/universities?search=${searchQuery}`);
+            
+                if (response.status === 200) setUniversities(response.universities);
+                else setUniversities([]);
+
+            } else setUniversities([]);
+            
+        }, 750);
+    
+        return () => clearTimeout(delayDebounceFn);
+
+    }, [searchQuery]);
+
+    const navigateToUni = (shortNameUni) => {
+        setSearchQuery('');
+        router.push(`/${shortNameUni}`)
     }
 
     return (
@@ -31,7 +48,8 @@ const UniFinder = () => {
                 <Combobox.Input
                     className="h-10 w-full border-0 bg-transparent pl-4 lg:pl-11 pr-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
                     placeholder="Buscar universidad"
-                    onChange={(e) => searchUni(e.target.value) }
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value) }
                 />
 
                 {
@@ -43,7 +61,7 @@ const UniFinder = () => {
                                 key={university.shortName}
                                 value={university.shortName}
                                 className='relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 hover:bg-gray-50'
-                                onClick={ () => router.push(`/${university.shortName}`) }
+                                onClick={ () => navigateToUni(university.shortName) }
                             >
                                 <div className='flex justify-start items-center hover:cursor-pointer'>
                                     <div>
