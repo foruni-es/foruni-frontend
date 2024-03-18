0ef7e15e9ff02e70add0294d572caa36b191929f
gobuperez <gobuperez@gmail.com>
2024-03-18 18:21:16 +0100

    Cambios UniHeader

--- src/components/UniHeader.js
+++ src/components/UniHeader.js
@@ -19,7 +19,7 @@ const UniHeader = ({ name, shortName }) => {
                 </>
                 :
                 <>
-                    <BuildingLibraryIcon className="h-7 w-7 mr-1.5 mb-2 inline-block" aria-hidden="true" />
+                    <BuildingLibraryIcon className="h-7 w-7 mr-2 mb-1.5 inline-block" aria-hidden="true" />
                     {`Foro ${shortName.toUpperCase()}`}
                 </>
             }
